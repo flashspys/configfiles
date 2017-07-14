1ff1b4d71ecc0ab07ee71571aef90f1c1b3a2ea2
felix <felix@wehnert.me>
2017-07-14 14:43:03 +0200

    add hash & remove readme

--- encrypt.sh
+++ encrypt.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-tar -cvhf files.tar files
+rhash --sha3-512 files.tar >> files.tar.sha3-512
+tar -chf files.tar files
 openssl aes-256-cbc -e -in files.tar -out files.tar.enc -pass file:key
+rhash --sha3-512 files.tar.enc >> files.tar.enc.sha3-512
